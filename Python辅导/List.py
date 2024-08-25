79f104c57b4197cbe96de466926a85875d6c32e4
jiayi <130427651+JerryPlus1@users.noreply.github.com>
2024-08-25 13:52:50 +0800

    updata Python辅导/List.py and
    Signed-off-by: jiayi <130427651+JerryPlus1@users.noreply.github.com>

---
+++ Python辅导/List.py
@@ -0,0 +1,23 @@
+number = [10,25,40,50]
+print(number)
+
+number.reverse()
+
+print(number)
+
+'''
+dictionary：
+
+'''
+
+tinydict = {'Alice': '2341', 'Beth': '9102', 'Cecil': '3258'}
+print(tinydict['Alice'])
+tinydict['s'] = 100
+print(tinydict)
+
+print(tinydict.get('Alice'))
+
+'''
+tuple
+'''
+t = ()
